8a47ab2c284d2de057c0e22696a556145bd5b6d1
pruthvir <31378049+pruthvir@users.noreply.github.com>
2017-08-26 21:21:42 -0400

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Set this to the root of your project when deployed:
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+http_generated_images_path = "../images"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
---
+++ startUp.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	//Initiating the foundation js
+	$(document).foundation({
+		orbit: {
+			animation: 'fade',
+			timer_speed: 3000,
+            pause_on_hover: true,
+            animation_speed: 500,
+            navigation_arrows: false,
+            slide_number:false
+		}
+	});	
+});
